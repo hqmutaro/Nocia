8ffbf558d30e757f70dff3b10821d2215b434209
Takaki Hamahiga <2tf2003@gmail.com>
2021-05-08 17:49:45 +0900

    [fix] conflict files

---
+++ lib/domain/news/news_id.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+@immutable
+class NewsId {
+  final String uuid;
+
+  NewsId(this.uuid);
+
+  @override
+  bool operator ==(Object other) =>
+      identical(other, this) || (other is NewsId && uuid == other.uuid);
+
+  @override
+  int get hashCode => uuid.hashCode;
+}
