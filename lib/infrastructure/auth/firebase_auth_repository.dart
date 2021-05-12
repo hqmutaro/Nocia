98bc66414d375e11df2a0b062163457cfa18fa33
Takaki Hamahiga <2tf2003@gmail.com>
2021-05-12 10:41:17 +0900

    [update] Implemented FirebaseAuthRepository

--- lib/infrastructure/auth/firebase_auth_repository.dart
+++ lib/infrastructure/auth/firebase_auth_repository.dart
@@ -1,29 +1,46 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:nocia/domain/auth/firebase_auth_repository_base.dart';
 import 'package:nocia/domain/auth/value/email_address.dart';
 import 'package:nocia/domain/auth/value/password.dart';
 
 class FirebaseAuthRepository implements FirebaseAuthRepositoryBase {
+  final FirebaseAuth _firebaseAuth;
+
+  FirebaseAuthRepository({required FirebaseAuth firebaseAuth})  : _firebaseAuth = firebaseAuth;
+
   @override
-  Future<void> getSignedInUser() {
-    // TODO: implement getSignedInUser
-    throw UnimplementedError();
+  Future<User?> getSignedInUser() async =>  _firebaseAuth.currentUser;
+
+  @override
+  Future<void> registerWithEmailAndPassword({
+    required EMailAddress email,
+    required Password password
+  }) async {
+    final emailValue = email.value;
+    final passwordValue = password.value;
+    await _firebaseAuth.createUserWithEmailAndPassword(
+        email: emailValue,
+        password: passwordValue
+    );
   }
 
   @override
-  Future<void> registerWithEmailAndPassword({required EMailAddress email, required Password password}) {
-    // TODO: implement registerWithEmailAndPassword
-    throw UnimplementedError();
+  Future<void> signInWithEmailAndPassword({
+    required EMailAddress email,
+    required Password password
+  }) async {
+    final emailValue = email.value;
+    final passwordValue = password.value;
+    await _firebaseAuth.signInWithEmailAndPassword(
+        email: emailValue,
+        password: passwordValue
+    );
   }
 
   @override
-  Future<void> signInWithEmailAndPassword({required EMailAddress email, required Password password}) {
-    // TODO: implement signInWithEmailAndPassword
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<void> signOut() {
-    // TODO: implement signOut
-    throw UnimplementedError();
+  Future<void> signOut() async {
+    await Future.wait([
+      _firebaseAuth.signOut()
+    ]);
   }
 }
