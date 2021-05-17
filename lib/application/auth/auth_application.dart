8928cdcf798eebb738b34e119ac1f0cf98655d2f
Takaki Hamahiga <2tf2003@gmail.com>
2021-05-17 11:00:05 +0900

    [update] パスワードリセットを要求する関数を追加

--- lib/application/auth/auth_application.dart
+++ lib/application/auth/auth_application.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:nocia/domain/auth/firebase_auth_repository_base.dart';
 import 'package:nocia/domain/auth/value/email_address.dart';
 import 'package:nocia/domain/auth/value/password.dart';
@@ -7,15 +8,22 @@ class AuthApplication {
 
   AuthApplication({required FirebaseAuthRepositoryBase repository}) : _authRepository = repository;
 
-  Future<void> registerWithEmailAndPassword(String emailStr, String passwordStr) async{
+  Future<void> registerWithEmailAndPassword(String emailStr, String passwordStr) async {
     final EmailAddress emailAddress = EmailAddress(emailStr);
     final Password password = Password(passwordStr);
     await _authRepository.registerWithEmailAndPassword(email: emailAddress, password: password);
   }
 
-  Future<void> signInWithEmailAndPassword(String emailStr, String passwordStr) async{
+  Future<void> signInWithEmailAndPassword(String emailStr, String passwordStr) async {
     final EmailAddress emailAddress = EmailAddress(emailStr);
     final Password password = Password(passwordStr);
     await _authRepository.signInWithEmailAndPassword(email: emailAddress, password: password);
   }
+
+  Future<void> sendPasswordResetEmail(String emailStr) async {
+    final EmailAddress emailAddress = EmailAddress(emailStr);
+    await _authRepository.sendPasswordResetEmail(email: emailAddress);
+  }
+
+  Future<void> signOut() async => await _authRepository.signOut();
 }
