646a1387674e42223e4d987828dd65d721b6e83a
Takaki Hamahiga <2tf2003@gmail.com>
2021-05-17 10:58:05 +0900

    [update] 認証画面UI

--- lib/presentation/auth/main.dart
+++ lib/presentation/auth/main.dart
@@ -1,84 +1,224 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/button_list.dart';
+import 'package:flutter_signin_button/button_view.dart';
+import 'package:nocia/application/user/user_notifier.dart';
+import 'package:nocia/presentation/auth/auth_check.dart';
+import 'package:nocia/presentation/home/page.dart';
 import 'package:nocia/presentation/notifier/auth/auth_notifier.dart';
 import 'package:provider/provider.dart';
 
 class Auth extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
     return Scaffold(
-      body: Column(
+        body: Container(
+          height: height,
+          child: Stack(
+            children: <Widget>[
+              Positioned(
+                top: -height * .15,
+                right: -MediaQuery.of(context).size.width * .4, child: Container(),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      SizedBox(height: height * .2),
+                      _title(),
+                      SizedBox(height: 50),
+                      Column(
+                        children: [
+                          _textField(context, "Eメール"),
+                          _textField(context, "パスワード", isPassword: true),
+                        ],
+                      ),
+                      SizedBox(height: 20),
+                      _createAccountLabel(context),
+                      SizedBox(height: 20),
+                      _submitButton(context),
+                      InkWell(
+                        child: Container(
+                          padding: EdgeInsets.symmetric(vertical: 10),
+                          alignment: Alignment.centerRight,
+                          child: Text(
+                              'パスワードを忘れましたか？',
+                              style: TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w500
+                              )
+                          ),
+                        ),
+                        onTap: () {},
+                      ),
+                      _divider(),
+                      SignInButton(
+                          Buttons.Google,
+                          text: "Googleでログイン",
+                          onPressed: () {
+
+                          }
+                      ),
+                      SizedBox(height: height * .055),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        )
+    );
+  }
+
+  Widget _textField(BuildContext context, title, {bool isPassword = false}) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Container(
-            height: 70,
-          ),
-          Padding(
-            padding: EdgeInsets.all(10),
-            child: TextField(
-              onChanged: (String value) {
-                context.read<AuthNotifier>().setEmailAddress(value);
-              },
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Eメール',
-                  hintText: 'メールアドレスを入力してください'
-              ),
+          Text(
+            title,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 15
             ),
           ),
-          Padding(
-            padding: EdgeInsets.all(10),
-            child: TextField(
-              onChanged: (String value) {
+          SizedBox(height: 10),
+          TextField(
+            onChanged: (String value) {
+              if (isPassword) {
                 context.read<AuthNotifier>().setPassword(value);
-              },
-              obscureText: true,
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'パスワード',
-                  hintText: 'パスワードを入力してください'
-              ),
-            ),
-          ),
-          TextButton(
-            onPressed: () async {
-              await context.read<AuthNotifier>().signInWithEmailAndPassword();
+              }
+              else {
+                context.read<AuthNotifier>().setEmailAddress(value);
+              }
             },
-            child: Text(
-              'パスワードを忘れました',
-              style: TextStyle(color: Colors.blue, fontSize: 15),
+            obscureText: isPassword,
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              fillColor: Color(0xfff3f3f4),
+              filled: true
             ),
-          ),
-          Container(
-            height: 50,
-            width: 250,
-            decoration: BoxDecoration(
-                color: Colors.blue, borderRadius: BorderRadius.circular(20)
-            ),
-            child: TextButton(
-              onPressed: () {},
-              child: Text(
-                'ログイン',
-                style: TextStyle(color: Colors.white, fontSize: 25),
-              ),
-            ),
-          ),
-          Container(
-            height: 50,
-            width: 250,
-            decoration: BoxDecoration(
-                color: Colors.white, borderRadius: BorderRadius.circular(20),
-            ),
-            child: TextButton(
-              onPressed: () async{
-                await context.read<AuthNotifier>().registerWithEmailAndPassword();
-              },
-              child: Text(
-                '新規登録',
-                style: TextStyle(color: Colors.blue, fontSize: 25),
-              ),
-            ),
-          ),
+          )
         ],
+      ),
+    );
+  }
+
+  Widget _submitButton(BuildContext context) {
+    return InkWell(
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 15),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(5)),
+            boxShadow: <BoxShadow>[
+              BoxShadow(
+                  color: Colors.grey.shade200,
+                  offset: Offset(2, 4),
+                  blurRadius: 5,
+                  spreadRadius: 2
+              )
+            ],
+            gradient: LinearGradient(
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight,
+                colors: [Color(0xfffbb448), Color(0xfff7892b)]
+            )
+        ),
+        child: Text(
+          'ログイン',
+          style: TextStyle(fontSize: 20, color: Colors.white),
+        ),
+      ),
+      onTap: () async{
+        await context.read<AuthNotifier>().signInWithEmailAndPassword();
+        context.read<UserNotifier>().fetchUser();
+        Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(builder: (context) => AuthCheck()), (_) => false
+        );
+      },
+    );
+  }
+
+  Widget _divider() {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      child: Row(
+        children: <Widget>[
+          SizedBox(width: 20),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: Divider(thickness: 1),
+            ),
+          ),
+          Text('or'),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: Divider(thickness: 1),
+            ),
+          ),
+          SizedBox(width: 20),
+        ]
       )
+    );
+  }
+
+  Widget _createAccountLabel(BuildContext context) {
+    return InkWell(
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 15),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(5)),
+            boxShadow: <BoxShadow>[
+              BoxShadow(
+                  color: Colors.grey.shade200,
+                  offset: Offset(2, 4),
+                  blurRadius: 5,
+                  spreadRadius: 2
+              )
+            ],
+            gradient: LinearGradient(
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight,
+                colors: [Colors.blue, Colors.blueAccent]
+            )
+        ),
+        child: Text(
+          '新規登録',
+          style: TextStyle(fontSize: 20, color: Colors.white),
+        ),
+      ),
+      onTap: () async{
+        await context.read<AuthNotifier>().registerWithEmailAndPassword();
+        context.read<UserNotifier>().fetchUser();
+        Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(builder: (context) => AuthCheck()), (_) => false
+        );
+      },
+    );
+  }
+
+  Widget _title() {
+    return RichText(
+      textAlign: TextAlign.center,
+      text: TextSpan(
+          text: 'd',
+          children: [
+            TextSpan(
+              text: 'Nocia',
+              style: TextStyle(color: Colors.blueAccent, fontSize: 30),
+            ),
+          ]
+      ),
     );
   }
 }
