432049c1a7b444a03d08739a6ebd4e5e04984214
Takaki Hamahiga <2tf2003@gmail.com>
2021-07-06 10:55:21 +0900

    [fix] モバイルページに遷移させる

--- lib/presentation/news/widgets/web_view_page.dart
+++ lib/presentation/news/widgets/web_view_page.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+import 'package:nocia/domain/news/service/rss_category.dart';
 
 class WebViewPage extends StatelessWidget {
   final String _url;
@@ -12,7 +13,7 @@ class WebViewPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: WebviewScaffold(
-        url: _url,
+        url: isPDF(_url) ? _url : "http://www.okinawa-ct.ac.jp/sp/" + _url.substring(27),// spをつけるとモバイルページに遷移する
         appBar: AppBar(
           title: Text(""),
         ), // AppBar
