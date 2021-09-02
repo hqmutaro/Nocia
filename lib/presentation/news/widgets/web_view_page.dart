3f1e4daf5565c277ff0a6484f6273919419bd824
Takaki Hamahiga <2tf2003@gmail.com>
2021-09-02 13:32:08 +0900

    [fix] AndroidでPDFをダウンロードする

--- lib/presentation/news/widgets/web_view_page.dart
+++ lib/presentation/news/widgets/web_view_page.dart
@@ -3,19 +3,43 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:nocia/domain/news/service/rss_category.dart';
+import 'package:flutter_cached_pdfview/flutter_cached_pdfview.dart';
 
 class WebViewPage extends StatelessWidget {
   final String _url;
+  final InAppWebViewGroupOptions _options = InAppWebViewGroupOptions(
+    crossPlatform: InAppWebViewOptions(
+      useOnDownloadStart: true,
+      useShouldOverrideUrlLoading: true,
+      mediaPlaybackRequiresUserGesture: false,
+      javaScriptEnabled: true,
+    ),
+    android: AndroidInAppWebViewOptions(
+      useHybridComposition: true,
+      loadWithOverviewMode: true,
+      useWideViewPort: false,
+      builtInZoomControls: false,
+      domStorageEnabled: true,
+      supportMultipleWindows: true,
+    ),
+  );
 
   WebViewPage({required String url})  : _url = url;
 
   @override
   Widget build(BuildContext context) {
+    var pdfUrl = "http://www.okinawa-ct.ac.jp/" + _url.substring(28);
+    var pageUrl = "http://www.okinawa-ct.ac.jp/sp/" + _url.substring(28);
+    print(pdfUrl);
     return Scaffold(
       appBar: AppBar(),
-      body: InAppWebView(
-        initialUrlRequest: URLRequest(url: Uri.parse(isPDF(_url) ? _url : "http://www.okinawa-ct.ac.jp/sp/" + _url.substring(29))),
-      ),
+      body: isPDF(_url)
+          ? PDF(pageFling: true).fromUrl(pdfUrl)
+          : InAppWebView(
+          initialOptions: _options,
+          initialUrlRequest: URLRequest(url: Uri.parse(pageUrl)
+          )
+      )
     );
   }
 }
